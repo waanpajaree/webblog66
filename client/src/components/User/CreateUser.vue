383d7001ccdf485832e26f47cd0c7f58ede5d306
waanpajaree <applekrnawee@gmail.com>
2023-09-27 14:42:51 +0700

    finish ch8

--- client/src/components/User/CreateUser.vue
+++ client/src/components/User/CreateUser.vue
@@ -1,12 +1,42 @@
 <template>
     <div>
-    <h1>Create User</h1>
+      <h1>Create User</h1>
+      <form v-on:submit.prevent = "createUser">
+        <p>ชื่อ: <input type="text" v-model="user.name"></p>
+        <p>นามสกุล: <input type="text" v-model="user.lastname"></p>
+        <p>email: <input type="text" v-model="user.email"></p>
+        <p>password: <input type="text" v-model="user.password"></p>
+        <p><button type="submit">create user</button></p>
+      </form>
     </div>
-   </template>
-   <script>
-   export default {
-    
-   }
-   </script>
-   <style scoped>
-   </style>
+  </template>
+  
+  <script>
+  import UsersService from "@/services/UsersService";
+  
+  export default {
+    data() {
+      return {
+        user: {
+          name: "",
+          lastname: "",
+          email: "",
+          password: "",
+          status: "active"
+        }
+      };
+    },
+    methods: {
+      async createUser(){
+        try{
+          await UsersService.post(this.user)
+          this.$router.push('/users')
+        }catch(err){
+          console.log(err)
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style></style>
--- client/src/components/User/Index.vue
+++ client/src/components/User/Index.vue
@@ -1,18 +1,56 @@
 <template>
     <div>
-        <h1>Get All Users</h1>
+      <h1>Get All Users</h1>
+      <div><h2>จำนวนผู้ใช้ {{ users.length }}</h2></div>
+      <div v-for="i in users" v-bind:key="i.id">
+        <div>ชื่อ: {{ i.name }}</div>
+        <div>นามสกุล: {{ i.lastname }}</div>
+        <div>status: {{ i.status }}</div>
+        <div>type: {{ i.type }}</div>
+        
+        <div><button v-on:click="navigateTo('/user/'+i.id)">ดูข้อมูลผู้ใช้</button></div>
+        <div><button @click="navigateTo('/user/edit/'+i.id)">edit user</button></div>
+        <div><button @click="deleteUser(i)">delete user</button></div>
+        <hr>
+      </div>
     </div>
-</template>
-
-<script>
-import UsersService from '@/services/UsersService'
-
-export default {
+  </template>
+  
+  <script>
+  import UsersService from "@/services/UsersService";
+  
+  export default {
+    data() {
+      return {
+        users: []
+      };
+    },
+    methods:{
+      navigateTo(route){
+        this.$router.push(route)
+      },
+      async deleteUser(user){
+        let result = confirm("Want of delete?")
+        if (result){
+          try{
+            await UsersService.delete(user)
+            this.refreshData()
+          }catch(err){
+            console.log(err)
+          }
+        }
+      }
+    },
     async created() {
-        let results = (await UsersService.index()).data
-        console.log(results)
+      try{
+        this.users = (await UsersService.index()).data;
+      }catch (err){
+        console.log(err)
+      }
+      
     }
-}
-</script>
-
+  };
+  </script>
+  
+  <style></style>
 <style scoped></style>
